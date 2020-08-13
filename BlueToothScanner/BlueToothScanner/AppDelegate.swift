f81fa0b4e0092af2e9fe7afc884c7dd8104e69b5
tuan.nguyenv <ext-tuan.nguyenv@spdigital.sg>
2020-08-13 16:22:17 +0700

    aa

--- BlueToothScanner/BlueToothScanner/AppDelegate.swift
+++ BlueToothScanner/BlueToothScanner/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         _ = BluetoothService.shared
-
+//
         return true
     }
 
